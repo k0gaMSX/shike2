e851a460e7e820eb7a1331daa0d2e769fa13b0b7
Roberto E. Vargas Caballero <k0ga@shike2.com>
2013-04-21 12:24:41 +0200

    Add game.asm skeleton
    Game.asm will contain the main loop of the game. In this moment we only
    can put a call to the editor.

--- src/ld.cmd
+++ src/ld.cmd
@@ -1,3 +1,3 @@
-shike2,vdp,kbd,bios,stdlib,zbuffer,level,mouse,editor,edlevel,edfloor,edtile,edmap,map,edroom
+shike2,vdp,kbd,bios,stdlib,zbuffer,game,level,mouse,editor,edlevel,edfloor,edtile,edmap,map,edroom
 
 
