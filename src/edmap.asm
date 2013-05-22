7bce61e1b1d752e573ead79ee09058efb9cab845
Roberto E. Vargas Caballero <k0ga@shike2.com>
2013-05-22 16:57:58 +0200

    Remove all the receivers are no longer used.
    We can not use the same code we were using until now because the map posiitons
    are isometric now, and the receivers can only catch rectangular regions, so
    we only can use a rectangular region which decides what map position is clicked.

--- src/edmap.asm
+++ src/edmap.asm
@@ -11,7 +11,6 @@
 	EXTRN	CLRVPAGE,CARTPAGE,EDINIT,VDPSYNC,LISTEN
 
 ED.MAP:	CALL	EDINIT
-	CALL	ADDRECEIVERS
 	LD	HL,0
 	LD	(COORD),HL
 
@@ -30,65 +29,11 @@ ED.LOOP:LD	E,EDPAGE
 	EXTRN	PALEVENT,SETEVENT
 
 RECEIVERS:
-	DB	1,29,142,8
+	DB	1,29,166,8
 	DW	ROOMEVENT
-	DB	1,29,150,8
+	DB	1,29,174,8
 	DW	HEIGHTEVENT
-	DB	220,16,126,16
-	DW	FLOOREVENT
-	DB	220,16,150,48
-	DW	TILEEVENT
-R.FLOOR:DS	64*6
 	DB	0
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	CSEG
-
-ADDRECEIVERS:
-	LD	HL,R.FLOOR
-	LD	(A.PTR),HL
-	LD	DE,0
-	LD	B,MAPYSIZ
-
-A.LOOPY:PUSH	BC			;RUN OVER Y
-	PUSH	DE
-	LD	B,MAPXSIZ
-
-A.LOOPX:PUSH	BC			;RUN OVER X
-	PUSH	DE
-	CALL	WRL2SCR			;TRANSFORM TO SCREEN COORDENATES
-	EX	DE,HL
-	LD	HL,(A.PTR)
-	INC	D			;AVOID X = 0
-	LD	(HL),D
-	INC	HL
-	LD	(HL),15
-	INC	HL
-	LD	(HL),E
-	INC	HL
-	LD	(HL),16
-	INC	HL
-	LD	DE,POSEVENT
-	LD	(HL),E
-	INC	HL
-	LD	(HL),D
-	INC	HL
-	LD	(A.PTR),HL
-	POP	DE
-	INC	D
-	POP	BC
-	DJNZ	A.LOOPX			;NEXT X
-
-	POP	DE
-	INC	E
-	POP	BC
-	DJNZ	A.LOOPY			;NEXT Y
-	RET
-
-	DSEG
-
-A.PTR:	DW	0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -117,16 +62,10 @@ GETMDATA:				;UPDATE THE MAP VARIABLES
 	EXTRN	DRAWSTACKS,EDTILE,EDFLOOR,GLINES,LOCATE,PRINTF
 	EXTRN	EDLEVEL,EDROOM,MAPMAP
 
-SHOWSCR:LD	DE,18
+SHOWSCR:LD	DE,21
 	CALL	LOCATE
 
 	LD	H,0
-	LD	A,(EDTILE)
-	LD	L,A
-	PUSH	HL
-	LD	A,(EDFLOOR)
-	LD	L,A
-	PUSH	HL
 	LD	DE,(EDLEVEL)
 	LD	L,E
 	PUSH	HL
@@ -142,7 +81,13 @@ SHOWSCR:LD	DE,18
 	PUSH	HL
 	LD	L,D
 	PUSH	HL
+	LD	A,(EDTILE)
+	LD	L,A
+	PUSH	HL
 	LD	A,(HEIGHT)
+	LD	L,A
+	PUSH	HL
+	LD	A,(EDFLOOR)
 	LD	L,A
 	PUSH	HL
 	LD	DE,(MAPNO)
@@ -156,159 +101,18 @@ SHOWSCR:LD	DE,18
 	RET
 
 
-FMT:	DB	" MAP",9," %04d",10
-	DB	" HEIGHT",9,"   %02d",9,"POS %d,%d",10
-	DB	" ROOM",9,"%02dX%02d",10
-	DB	" LEVEL",9,"%02dX%02d",10
-	DB	" FLOOR",9,"  %03d",10
-	DB	" TILE",9,"  %03d",0
+FMT:	DB	" MAP",9," %04d FLOOR",9,"%03d",10
+	DB	" HEIGHT",9,"   %02d TILE",9,"%03d",10
+	DB	" POS=%03dX%03d,ROOM=%02dX%02d,LEVEL=%02dX%02d",0
 
 ;	       REP  X0  Y0    X1  Y1 IX0 IY0 IX1 IY1
 MAPG:	DB     	9,  0,  95,  127, 32, 16,  8, 16,  8
 	DB	9,  0,  96,  127,159, 16, -8, 16, -8
-	DB	3,  0, 142,   30,142,  0,  8,  0,  8
-	DB	2,  0, 142,    0,158, 30,  0, 30,  0
-	DB	2,220, 126,  236,126,  0, 16,  0, 16
-	DB	2,220, 126,  220,142, 16,  0, 16,  0
-	DB	2,220, 150,  236,150,  0, 48,  0, 48
-	DB	2,220, 150,  220,198, 16,  0, 16,  0
+	DB	3,  0, 166,   30,166,  0,  8,  0,  8
+	DB	2,  0, 166,    0,182, 30,  0, 30,  0
+	DB	3,  54, 166,  84,166,  0,  8,  0,  8
+	DB	2,  54, 166,  54,182, 30,  0, 30,  0
 	DB	0
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;INPUT:	DE = SCREEN COORDINATES
-;OUTPUT:HL = WORLD COORDENATES
-
-	CSEG
-
-ISOX	EQU	CENTRAL.P1X*16
-ISOY	EQU	CENTRAL.P1Y*8
-
-SCR2WRL:SRL	E
-	SRL	E
-	SRL	E			;E = Ys/8
-	LD	A,E
-	SUB	ISOY/8
-	LD	E,A			;E = Y' = (Ys - ISOY)/8
-	SLA	E
-	SLA	E			;E = 4Y'
-	SRL	D
-	SRL	D
-	SRL	D			;D = Xs/8
-	LD	A,D
-	AND	0FEH			;CLEAN LOWER BIT, BECAUSE WE WANT
-	SUB	ISOX/8			;LEFT UP CORNERS
-	LD	D,A			;D = X' = (Xs - ISOX)/8
-	SLA	D			;D = 2X'
-
-	LD	A,E
-	ADD	A,D			;A = 4Y' + 2X'
-	SRA	A
-	SRA	A
-	SRA	A
-	LD	H,A			;H = Xw = (4Y' + 2X')/8
-
-	LD	A,E
-	SUB	D			;A = 4Y' - 2X'
-	SRA	A
-	SRA	A
-	SRA	A
-	LD	L,A			;L = Yw = (4Y' - 2X')/8
-	RET
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;INPUT:	DE = WORLD COORDENATES
-;OUTPUT:HL = SCREEN COORDENATES
-	CSEG
-
-WRL2SCR:LD	A,D
-	SUB	E
-	ADD	A,A
-	ADD	A,A
-	ADD	A,A
-	ADD	A,A			;X' = (Xw-Yw)*16
-	ADD	A,ISOX
-	LD	H,A			;Xs = X' + ISOX
-
-	LD	A,E
-	ADD	A,D
-	ADD	A,A
-	ADD	A,A
-	ADD	A,A			;Y' = (Xw+Yw)*8
-	ADD	A,ISOY
-	LD	L,A			;Ys = Y' + ISOY
-	RET
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;INPUT:	A = EVENT
-;	DE = PARAMETER (IN MOUSE EVENTS = SCREEN COORDENATES)
-
-	CSEG
-	EXTRN	EDROOM,ADDAHL,EDTILE,EDFLOOR,GETTMAP,GETFMAP,GETHMAP
-
-POSEVENT:
-	EX	AF,AF'
-	LD	HL,(MAPNO)		;MAP 0 IS THE EMPTY MAP
-	LD	A,L
-	OR	H			;DON'T LET TO THE USER MODIFY IT
-	RET	Z
-	EX	AF,AF'
-
-	PUSH	AF
-	CALL	SCR2WRL			;TRANSFORM TO WORLD COORDENATES
-	LD	(P.COORD),HL
-	LD	A,L
-	ADD	A,A
-	ADD	A,A
-	ADD	A,A
-	ADD	A,H
-	LD	(P.OFFSET),A		;CALCULATE OFFSET IN THE MAP
-	POP	AF
-	CP	MS_BUTTON1
-	JR	NZ,P.TILE
-
-	LD	DE,(MAPNO)
-	CALL	GETFMAP			;BUTTON 1 SET FLOOR
-	LD	A,(P.OFFSET)
-	CALL	ADDAHL
-	LD	A,(EDFLOOR)
-	LD	(HL),A
-	RET
-
-P.TILE:	CP	MS_BUTTON2		;BUTTON 2 SET TILE
-	JR	NZ,P.INFO
-
-	LD	DE,(EDTILE)		;GET THE HEIGHT OF THE TILE
-	CALL	GETTILE
-	EX	DE,HL
-	LD	IYL,E
-	LD	IYU,D
-	LD	A,(IY+TILE.HEIGHT)
-	PUSH	AF
-	LD	DE,(MAPNO)
-	CALL	GETHMAP			;GET THE HEIGHT MAP
-	LD	A,(P.OFFSET)
-	CALL	ADDAHL
-	POP	AF
-	LD	(HL),A
-
-	LD	DE,(MAPNO)
-	CALL	GETTMAP			;BUTTON 2 SET TILE
-	LD	A,(P.OFFSET)
-	CALL	ADDAHL
-	LD	A,(EDTILE)
-	LD	(HL),A
-	RET
-
-P.INFO:	CP	KB_F1
-	RET	NZ
-	LD	HL,(P.COORD)
-	LD	(COORD),HL
-	RET
-
-	DSEG
-P.OFFSET:	DB	0
-P.COORD:	DW	0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;INPUT:	A = EVENT
@@ -360,22 +164,6 @@ H.1:	CP	MS_BUTTON2
 	DEC	A
 H.RET:	LD	(HEIGHT),A
 	RET
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	CSEG
-	EXTRN	EDINIT
-
-FLOOREVENT:
-	JP	EDINIT
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-	CSEG
-	EXTRN	EDINIT
-
-TILEEVENT:
-	JP	EDINIT
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
