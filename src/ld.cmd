de262c7d77be4d81092d16fb1944165b6c71ea62
Roberto E. Vargas Caballero <k0ga@shike2.com>
2013-06-05 13:22:29 +0200

    Add doors skeleton to the game
    This is the first version of the doors and it is not full working.

--- src/ld.cmd
+++ src/ld.cmd
@@ -1,3 +1,3 @@
-shike2,vdp,kbd,bios,stdlib,zbuffer,char,movable,mob,game,level,talk,edtile,edfloor,mouse,editor,edlevel,edmap,map,edroom
+shike2,vdp,kbd,bios,stdlib,zbuffer,char,movable,mob,game,level,door,talk,edtile,edfloor,mouse,editor,edlevel,edmap,map,edroom
 
 
