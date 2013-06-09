ba9a13fc8cdbc5236d38fec6f8e17d8c7cb30e27
Roberto E. Vargas Caballero <k0ga@shike2.com>
2013-06-09 11:30:54 +0200

    Add skeleton for object.asm
    This is only a skeleton version of object.

---
+++ src/object.asm
@@ -0,0 +1,49 @@
+
+	INCLUDE	DATA.INC
+
+
+NR_OBJECTS	EQU	6
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;INPUT:	IX = POINTER TO THE OBJECT
+;	DE = POINT LOCATION
+;	C = OBJECT ID
+
+	CSEG
+	PUBLIC	OBJECT
+	EXTRN	MOVABLE,PLACE,ADDAHL
+
+OBJECT:	PUSH	BC
+	PUSH	DE
+	LD	HL,OBJDEF
+	LD	A,C
+	CALL	ADDAHL
+	LD	E,(HL)			;E = PATTERN
+	INC	HL
+	LD	C,(HL)			;C = SIZE
+	LD	HL,0
+	CALL	MOVABLE			;INIT MOVABLE
+
+	POP	DE
+	LD	C,DNODIR
+	CALL	PLACE
+
+	POP	BC
+	LD	(IX+OBJECT.ID),C
+	LD	(IX+OBJECT.OWNER),-1
+	LD	E,IXL
+	LD	D,IXU
+	LD	(IX+OBJECT.NEXT),E
+	LD	(IX+OBJECT.NEXT+1),D
+	LD	(IX+OBJECT.PREV),E
+	LD	(IX+OBJECT.PREV+1),D
+	RET
+
+
+OBJDEF:	DB	0,0			;PATTERN,SIZE
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
